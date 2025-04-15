c822d0f5b2aed02e8916d9d6f76ccb1c1ff59338
Marco <marco.madrid@utec.edu.pe>
2025-04-14 20:21:16 -0500

    espero que funcione

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -8,28 +8,40 @@ class MVStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        vpc = ec2.Vpc(
+            self,
+            "MinimalVPC",
+            max_azs=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Public",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24
+                )
+            ]
+        )
+
         sg = ec2.SecurityGroup(
-            self, 
+            self,
             "SG",
+            vpc=vpc,
             description="Permitir acceso SSH y HTTP",
-            allow_all_outbound=True,
-            vpc=None
+            allow_all_outbound=True
         )
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "SSH")
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "HTTP")
 
         ec2.Instance(
-            self, 
+            self,
             "Instancia",
             instance_type=ec2.InstanceType("t2.micro"),
             machine_image=ec2.MachineImage.lookup(name="Cloud9ubuntu22"),
             key_name="vockey",
+            vpc=vpc,
             security_group=sg,
             block_devices=[
                 ec2.BlockDevice(
                     device_name="/dev/xvda",
-                    volume=ec2.BlockDeviceVolume.ebs(20)
-                )
-            ],
-            vpc=None
+                    volume=ec2.BlockDeviceVolume.ebs(20))
+            ]
         )
