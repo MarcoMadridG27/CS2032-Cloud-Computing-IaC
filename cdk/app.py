e0877690a6656242902ab0ecdd16f031a68fcadd
Marco <marco.madrid@utec.edu.pe>
2025-04-14 20:27:11 -0500

    falto region y cuenta

--- cdk/app.py
+++ cdk/app.py
@@ -3,5 +3,7 @@ import aws_cdk as cdk
 from cdk_stack import MVStack
 
 app = cdk.App()
-MVStack(app, "MVDesarrollo")
+MVStack(app, "MVDesarrollo",
+    env=cdk.Environment(account="584758245304", region="us-east-1")
+)
 app.synth()
