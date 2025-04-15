c0822d757fce435f1e6165b99e1f8c4033dba0c7
Marco <marco.madrid@utec.edu.pe>
2025-04-14 21:14:07 -0500

    role...

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -8,6 +8,7 @@ class MVStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Crear VPC con subnet pública
         vpc = ec2.Vpc(self, "VPC",
             max_azs=1,
             subnet_configuration=[
@@ -20,6 +21,10 @@ class MVStack(Stack):
             nat_gateways=0
         )
 
+        # Obtener una subred pública de la VPC
+        public_subnet = vpc.public_subnets[0]
+
+        # Grupo de seguridad
         sg = ec2.SecurityGroup(
             self, "SG",
             vpc=vpc,
@@ -29,20 +34,25 @@ class MVStack(Stack):
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "SSH")
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "HTTP")
 
-        key_pair = ec2.KeyPair.from_key_pair_name(self, "Vockey", "vockey")
+        # AMI Ubuntu (la que mencionaste antes)
+        ami_id = "ami-03c983b9d7cd45421"  # Cloud9ubuntu22
 
-        ec2.Instance(
-            self, "Instancia",
-            instance_type=ec2.InstanceType("t2.micro"),
-            machine_image=ec2.MachineImage.latest_amazon_linux(),
-            vpc=vpc,
-            security_group=sg,
-            key_pair=key_pair,
-            init_options=ec2.InstanceInitOptions(config=None),  # 🚫 evitar creación de rol IAM
-            block_devices=[
-                ec2.BlockDevice(
-                    device_name="/dev/xvda",
-                    volume=ec2.BlockDeviceVolume.ebs(20)
-                )
-            ]
+        # Instancia EC2 con CfnInstance
+        ec2.CfnInstance(self, "Instancia",
+            image_id=ami_id,
+            instance_type="t2.micro",
+            key_name="vockey",
+            subnet_id=public_subnet.subnet_id,
+            security_group_ids=[sg.security_group_id],
+            block_device_mappings=[{
+                "deviceName": "/dev/xvda",
+                "ebs": {
+                    "volumeSize": 20,
+                    "volumeType": "gp2"
+                }
+            }],
+            tags=[{
+                "key": "Name",
+                "value": "MVDesarrollo"
+            }]
         )
